ac8f10def9a5ea3074d3b014201c8050d2679b5c
elidvenega <venegaelid@gmail.com>
2022-04-02 19:55:16 -0700

    packages updated

--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
