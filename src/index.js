23e323ba5026476687b9b4396ee41447fc5d3bc1
elidvenega <venegaelid@gmail.com>
2022-09-30 14:40:08 -0700

    Worked on SCSS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './scss/style.scss';
+import './style.scss';
 import App from "./App";
 import { ContactForm } from "./components/ContactForm";
 
