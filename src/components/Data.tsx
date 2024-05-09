8b2811859f56e71b691fb01124f74369f0cdf5f6
elidvenega <robotsmailNinja@gmail.com>
2024-05-08 23:23:08 -0700

    Worked on the footer links and got it to loop

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -10,7 +10,7 @@ export interface ImageInfo {
   
   export interface LinkInfo {
     href: string;
-    content?: ImageInfo | string;
+    content?:  string;
   }
   
   export interface BlockInfo {
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,56 +1,45 @@
-// import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
 import { Content } from "./Data.tsx";
 
 export const Footer = () => {
-  // const twitter:string = "https://twitter.com";
-  // const facebook:string = "https://www.facebook.com/";
-  // const instagram:string = "https://www.instagram.com/";
-
   const footerLinks: Content = {
     cellBlocks: [
       {
         link: {
           href: "https://www.facebook.com/",
+          content: "fab fa-facebook fa-2x",
         },
       },
       {
         link: {
           href: "https://www.instagram.com/",
+          content: "fab fa-instagram fa-2x",
         },
       },
       {
         link: {
           href: "https://twitter.com/",
+          content: "fab fa-twitter fa-2x",
         },
-      }
+      },
     ],
   };
 
-  
-const firstHref = footerLinks.cellBlocks[0]?.link?.href;
-const secondHref = footerLinks.cellBlocks[1]?.link?.href;
-const thirdHref = footerLinks.cellBlocks[2]?.link?.href;
-const allLinks: any = [firstHref, secondHref,thirdHref]
   return (
-    <>
-      <footer>
-        <div className="links">
-          {/* <a href={facebook} target="_blank" rel="noreferrer">
-            <i className="fab fa-facebook fa-2x"></i>
-          </a>
-          <a href={instagram} target="_blank" rel="noreferrer">
-            <i className="fab fa-instagram fa-2x"></i>
-          </a>
-          <a href={twitter} target="_blank" rel="noreferrer">
-            <i className="fab fa-twitter fa-2x"></i>
-          </a> */}
-          <ul>
-          {
-            allLinks.map((link: string ) => <a href={link}>{link}</a>)
-          }
-          </ul>
-        </div>
-      </footer>
-    </>
+    <footer>
+      <div className="links">
+        <ul>
+          {footerLinks.cellBlocks.map((cellBlock, index) =>
+            // Check if `cellBlock.link` is defined before accessing it
+            cellBlock.link ? (
+              <li key={index}>
+                <a href={cellBlock.link.href} target="_blank" rel="noreferrer">
+                  <i className={cellBlock.link.content}></i>
+                </a>
+              </li>
+            ) : null // If `cellBlock.link` is undefined, do nothing
+          )}
+        </ul>
+      </div>
+    </footer>
   );
 };
