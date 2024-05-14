be36aed84b0854aa205f65f3091bdb91ce14d213
elidvenega <robotsmailNinja@gmail.com>
2024-05-13 23:52:50 -0700

    Worked on React Portfolio Project

--- src/components/MusicInfo.tsx
+++ src/components/MusicInfo.tsx
@@ -1,16 +1,14 @@
 export const MusicInfo = () => {
   return (
-    <>
-      <div className="music" id="music">
-        <div id="layer"></div>
-        <h1 className="caption text-center white z-index">Music</h1>
-        <p className="lead white z-index text-center lead">
-          We sell music records of all genres so I'm sure you will find
-          something you like. I recommend to check our catalog our team is
-          professional and will assist with any questions you may have.
-        </p>
-        <button className="button">Music</button>
-      </div>
-    </>
+    <div className="music" id="music">
+      <div id="layer"></div>
+      <h1 className="caption text-center white z-index">Music</h1>
+      <p className="lead white z-index text-center lead">
+        We sell music records of all genres so I'm sure you will find something
+        you like. I recommend to check our catalog our team is professional and
+        will assist with any questions you may have.
+      </p>
+      <button className="button">Music</button>
+    </div>
   );
 };
