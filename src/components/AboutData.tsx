7a56731776fc2d5c2357719113286efc730d2719
elidvenega <robotsmailNinja@gmail.com>
2024-05-21 19:45:45 -0700

    Updated

--- src/components/AboutData.tsx
+++ src/components/AboutData.tsx
@@ -1,13 +1,25 @@
-export const AboutData = () => (
-  <section className=" container">
-    <h1 className="caption text-center">About</h1>
-    <p className="lead">
-      Euphony records was founded because of my love for music wanting to
-      provide music records of different genres to people I decided to open a
-      record store. We host local acts on weekends to give them exposure, as
-      well as sponsor many community events as a way to give back to the
-      community. Our mission is just to expose to bring people together through
-      music.
-    </p>
-  </section>
-);
+import { Content } from "./Data.tsx";
+
+export const AboutData = () => {
+  const aboutContent: Content = {
+    cellBlocks: [
+      {
+        heading: `About`,
+        content: `  Euphony records was founded because of my love for music wanting to
+        provide music records of different genres to people I decided to open a
+        record store. We host local acts on weekends to give them exposure, as
+        well as sponsor many community events as a way to give back to the
+        community. Our mission is just to expose to bring people together
+        through music.`,
+      },
+    ],
+  };
+  return (
+    <section className=" container">
+      <h1 className="caption text-center">
+        {aboutContent.cellBlocks[0].heading}
+      </h1>
+      <p className="lead">{aboutContent.cellBlocks[0].content}</p>
+    </section>
+  );
+};
--- src/components/HomeInfo.tsx
+++ src/components/HomeInfo.tsx
@@ -1,4 +1,3 @@
-
 import { AboutData } from "./AboutData.tsx";
 import { Content } from "./Data.tsx";
 
@@ -11,13 +10,16 @@ export const HomeInfo = () => {
         button: `Music`,
       },
       {
-        title: `About`,
-        content: `  Euphony records was founded because of my love for music wanting to
-        provide music records of different genres to people I decided to open
-        a record store. We host local acts on weekends to give them exposure,
-        as well as sponsor many community events as a way to give back to the
-        community. Our mission is just to expose to bring people together
-        through music.`,
+        title: `Acoustic`,
+      },
+      {
+        title: `All Genres`,
+      },
+      {
+        title: `Rock`,
+      },
+      {
+        title: `Careers`,
       },
     ],
   };
@@ -30,25 +32,20 @@ export const HomeInfo = () => {
 
         <button className="button">{homeContent.cellBlocks[0].button}</button>
       </div>
-      {/* <section id="about" className="container">
-        <h2 className="caption text-center">
-          {homeContent.cellBlocks[1].title}
-        </h2>
-        <p className="lead">{homeContent.cellBlocks[1].content}</p>
-      </section> */}
+
       <AboutData />
       <div className="squares" id="selection">
         <div className="square">
-          <h2>Acoustic</h2>
+          <h2>{homeContent.cellBlocks[1].title}</h2>
         </div>
         <div className="square-1">
-          <h2>All Genres</h2>
+          <h2>{homeContent.cellBlocks[2].title}</h2>
         </div>
         <div className="square-2">
-          <h2>Rock</h2>
+          <h2>{homeContent.cellBlocks[3].title}</h2>
         </div>
         <div className="square-3">
-          <h2>Careers</h2>
+          <h2>{homeContent.cellBlocks[4].title}</h2>
         </div>
       </div>
     </>
